9fe6fa46f9639921656e4c606ee5a5dd7334e6ea
mac <marko@hankkila.me>
2015-06-06 23:00:57 +0300

    Reformat code.

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,44 +1,20 @@
 <?php
 
-/*
-|--------------------------------------------------------------------------
-| Application Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register all of the routes for an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-get('guestbook', function() {
+get('guestbook', function () {
 
     return view('guestbook');
-
 
 });
 
 // API
 
-get('api/messages', function() {
+get('api/messages', function () {
 
     return App\Message::all();
-
 });
 
-post('api/messages', function() {
+post('api/messages', function () {
 
     App\Message::create(Request::all());
 
 });
-
-/*
-Route::get('/', 'WelcomeController@index');
-
-Route::get('home', 'HomeController@index');
-
-Route::controllers([
-	'auth' => 'Auth\AuthController',
-	'password' => 'Auth\PasswordController',
-]);
-*/
